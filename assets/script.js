b208e9f575884652646368388bbafd9a654e5705
Brandon Goose <beegee54321@gmail.com>
2024-09-12 20:29:26 -0700

    added functionality to js file

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,22 @@
 var wins=0;
 var ties=0;
 var losses=0;
+
+var options = ['Rock', 'Paper', 'Scissor'];
+
+var playGames = function() {
+    var userChoice = window.prompt('Enter Rock, Paper, or Scissor:');
+
+    if (!userChoice) {
+        return;
+    }
+
+    userChoice=userChoice.toUpperCase();
+
+    var index = Math.floor(Math.random() * options.length);
+    var computerChoice = options[index];
+
+    window.alert('The computer chose ' + computerChoice);
+
+    
+}
